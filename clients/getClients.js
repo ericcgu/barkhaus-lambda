ded362b06e3ce29a065ac7cebb872470509804bd
Eric Gu <eric.changning.gu@gmail.com>
2018-03-12 00:59:20 -0400

    completed getClients

--- clients/getClients.js
+++ clients/getClients.js
@@ -3,12 +3,12 @@
 const db = require("../db.js")
 
 module.exports.getClients = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   db.any('SELECT * FROM public."Clients";')
   .then(function (data) {
     const response = {
       statusCode: 200,
-      body: data
-      ,
+      body: JSON.stringify(data)
     };
     callback(null, response);
   })
