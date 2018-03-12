6b2df95483752b95c14fdc793d33c667485bc02c
Eric Gu <eric.changning.gu@gmail.com>
2018-03-11 22:41:46 -0400

    added secret config + pg promise + getClients

--- clients/getClients.js
+++ clients/getClients.js
@@ -1,16 +1,20 @@
 'use strict';
 
+const db = require("../db.js")
+
+const message = db.getClients
+
 module.exports.getClients = (event, context, callback) => {
-  const message = 'Thanks for hitting the get route.';
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: message
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  db.any('SELECT * FROM public."Clients";')
+  .then(function (data) {
+    const response = {
+      statusCode: 200,
+      body: data
+      ,
+    };
+    callback(null, response);
+  })
+  .catch(function (err) {
+    return next(err);
+  });
 };
