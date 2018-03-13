5c74f92729aa32f8f1704c7b1308468b59df24de
Eric Gu <eric.changning.gu@gmail.com>
2018-03-13 01:12:06 -0400

    added deleteClient route

---
+++ clients/deleteClient.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const db = require("../db.js")
+
+module.exports.deleteClient = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const ClientID = parseInt(event.pathParameters.ClientID);
+    db.result('DELETE FROM public."Clients" WHERE "ClientID" = $1', ClientID)
+        .then(function (result) {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(result.rowCount)
+            };
+            callback(null, response);
+        })
+        .catch(function (err) {
+            return err;
+        });
+};
