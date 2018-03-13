b619ed1c2ca77b81d7b61028e931c110b1567fa4
Eric Gu <eric.changning.gu@gmail.com>
2018-03-13 01:12:43 -0400

    changed minor cosmetic

--- clients/getClient.js
+++ clients/getClient.js
@@ -5,7 +5,7 @@ const db = require("../db.js")
 module.exports.getClient = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
     const ClientID = parseInt(event.pathParameters.ClientID);
-    db.one('SELECT * from public."Clients" where "ClientID" = $1', ClientID)
+    db.one('SELECT * from public."Clients" WHERE "ClientID" = $1', ClientID)
         .then(function (data) {
             const response = {
                 statusCode: 200,
