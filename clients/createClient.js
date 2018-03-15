98c6f1174f75991a447f1adff25e4f046d12b114
Eric Gu <eric.changning.gu@gmail.com>
2018-03-14 21:40:55 -0400

    completed create route

--- clients/createClient.js
+++ clients/createClient.js
@@ -1,19 +1,18 @@
 'use strict';
-
 const db = require("../db.js")
-
 module.exports.createClient = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
     const body = JSON.parse(event.body);
     db.none('INSERT INTO public."Clients"("FirstName", "LastName") VALUES(${FirstName}, ${LastName})',
-      body)
-      .then(function (result) {
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(result)
-        };
-        callback(null, response);
-    })
-    .catch(function (err) {
-      return err;
-    });
+            body)
+        .then(function () {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(body)
+            };
+            callback(null, response);
+        })
+        .catch(function (err) {
+            return err;
+        });
 };
